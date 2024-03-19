7ab0f881be96badb653ed0548bec485eec72b3eb
frajdzia <s.lidia@interia.pl>
2024-03-19 22:16:09 +0000

    footer works only at the bottom

--- js_main.js
+++ js_main.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     var footer = document.getElementById('footer');
+    var mainContent = document.querySelector('main');
+
     window.addEventListener('scroll', function() {
         // If scrolled to the bottom
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        if ((window.innerHeight + window.scrollY) >= mainContent.offsetHeight) {
             footer.style.display = 'block';
         } else {
             footer.style.display = 'none';
